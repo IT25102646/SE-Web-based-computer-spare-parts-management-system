ee6692ce09667d7669fd63a49f081d9bb41c8fb5
Ziona <it25102646@my.sliit.lk>
2026-07-26 21:30:51 +0530

    Initial project setup: pom.xml, shared layout and theme, base structure

---
+++ src/main/java/com/comspare/ComspareApplication.java
@@ -0,0 +1,11 @@
+package com.comspare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComspareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComspareApplication.class, args);
+    }
+}
